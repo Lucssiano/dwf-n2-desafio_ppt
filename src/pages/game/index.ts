01325d90201122de7b6a5c0e10b436004e67da42
Luciano Riente <lucianotomasriente@gmail.com>
2024-02-14 17:00:56 -0300

    Agrego funcionalidad de la computadora y resumo varias acciones en funciones

--- src/pages/game/index.ts
+++ src/pages/game/index.ts
@@ -3,53 +3,71 @@ export function gamePage(params) {
 	div.classList.add('game-container');
 
 	div.innerHTML = `
-		<div class="hands-game-container rotate">
+		<div class="hands-game-container computer-hands rotate">
 			<custom-hand type="rock" class="computer-hand"></custom-hand>
 			<custom-hand type="paper" class="computer-hand"></custom-hand>
 			<custom-hand type="scissors" class="computer-hand"></custom-hand>
 		</div>
 		<div class="timer-container">
 			<div class="loader"></div>
-			<div class="timer__counter">3</div>
+			<div class="timer__counter"></div>
 		</div>
-		<div class="hands-game-container">
+		<div class="hands-game-container user-hands">
 			<custom-hand type="rock" class="user-hand"></custom-hand>
 			<custom-hand type="paper" class="user-hand"></custom-hand>
 			<custom-hand type="scissors" class="user-hand"></custom-hand>
 		</div>
   `;
 
-	const timerContainer = div.querySelector('.timer-container');
-	const timerCounter = div.querySelector('.timer__counter');
+	const userHandsEl = div.querySelector('.user-hands')?.querySelectorAll('custom-hand') || []; // Esto es un NodeList // Tengo que poner el || [] porque sino me marca como que puede ser undefined
+	const userHandsArray = Array.from(userHandsEl); // Transformo la NodeList en un Array
+
+	const timerContainerEl = div.querySelector('.timer-container');
+	const timerCounterEl = div.querySelector('.timer__counter');
 	let counter = 3;
+
+	const computerHandsEl = div.querySelector('.computer-hands')?.querySelectorAll('custom-hand') || []; // Esto es un NodeList // Tengo que poner el || [] porque sino me marca como que puede ser undefined
+	const computerHandsArray = Array.from(computerHandsEl); // Transformo la NodeList en un Array
 
 	const interval = setInterval(() => {
 		/* Se queda mucho tiempo con el 3 y no avanza como por 2 segundos, averiguar por qué */
-		if (timerCounter) timerCounter.textContent = counter.toString();
+		if (timerCounterEl) timerCounterEl.textContent = counter.toString();
 		counter--;
 		if (counter < 0) {
-			timerContainer?.remove();
+			timerContainerEl?.remove();
+			checkHandMovement(userHandsArray);
+			makeComputerMovement(computerHandsArray);
 			clearInterval(interval);
 		}
-		/* Tengo que agregar que pasa si el usuario no presiona ninguna mano */
-		/* Tengo que agregar la jugada aleatoria de la computadora */
+		/* Ver de agregar contador durante la partida, tanto para el usuario cómo para la computadora */
 		/* Tengo que agregar el uso del state */
 	}, 1000);
 
 	// <div class="results-container"></div>
 
-	const hands = div.querySelectorAll('custom-hand'); // Esto es un NodeList
-	const handsArray = Array.from(hands);
-
-	handsArray.forEach((hand) => {
-		hand.addEventListener('click', () => {
-			hand.classList.add('active');
-			const inactiveHands = handsArray.filter((h) => h !== hand);
-			inactiveHands.forEach((inactiveHand) => inactiveHand.classList.add('inactive'));
-			const handType = hand.getAttribute('type') || 'rock';
-			hand.classList.add(handType);
-		});
-	});
+	userHandsArray.forEach((hand) => hand.addEventListener('click', () => toggleHandClasses(userHandsArray, hand)));
 
 	return div;
+}
+
+function checkHandMovement(handsArray) {
+	const noHandIsActive = handsArray.every((hand) => !(hand as Element).classList.contains('active'));
+	if (noHandIsActive) {
+		const paperHand = handsArray.find((hand) => (hand as Element).getAttribute('type') == 'paper');
+		toggleHandClasses(handsArray, paperHand);
+	}
+}
+
+function toggleHandClasses(handsArray, activeHand) {
+	activeHand.classList.add('active');
+	const inactiveHands = handsArray.filter((h) => h !== activeHand);
+	inactiveHands.forEach((inactiveHand) => inactiveHand.classList.add('inactive'));
+	const handType = activeHand.getAttribute('type') || 'rock';
+	activeHand.classList.add(handType);
+}
+
+function makeComputerMovement(computerHandsArray) {
+	const numberBetween0and2 = Math.floor(Math.random() * 3);
+	const handToMove = computerHandsArray[numberBetween0and2];
+	toggleHandClasses(computerHandsArray, handToMove);
 }
--- src/pages/rules/index.ts
+++ src/pages/rules/index.ts
@@ -8,7 +8,7 @@ export function rulesPage(params) {
               Presioná jugar y elegí: piedra, papel o tijera antes de que pasen los 3 segundos.
           </p>
           <p class="paragraph">
-             Te deseamos suerte!
+            Gana quien llega a 3 manos ganadas o gana dos manos seguidas.
           </p>
           <custom-button>¡Jugar!</custom-button>
         </div>
